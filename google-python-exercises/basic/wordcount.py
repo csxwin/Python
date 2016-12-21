2e83d35dcf45b42974fb7dd2be674f3ebc14e22f
csxwin <caoshuxun@gmail.com>
2016-12-20 22:22:34 -0500

    function decleration and print correction

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -44,6 +44,14 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
+def print_words(filename)
+
+
+
+
+
+def print_top(filename)
+
 
 ###
 
@@ -51,7 +59,7 @@ import sys
 # calls the print_words() and print_top() functions which you must define.
 def main():
   if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
+    print ('usage: ./wordcount.py {--count | --topcount} file')
     sys.exit(1)
 
   option = sys.argv[1]
@@ -61,7 +69,7 @@ def main():
   elif option == '--topcount':
     print_top(filename)
   else:
-    print 'unknown option: ' + option
+    print ('unknown option: ' + option)
     sys.exit(1)
 
 if __name__ == '__main__':
