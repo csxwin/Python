27ed9c38f5d53ebb47fa02f99e157954a110532d
csxwin <caoshuxun@gmail.com>
2016-12-20 22:24:33 -0500

    open file

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -45,7 +45,7 @@ import sys
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
 def print_words(filename)
-
+#open filename
 
 
 
